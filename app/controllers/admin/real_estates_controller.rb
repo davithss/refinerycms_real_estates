8a9a9afdab0eb5befa892d8697876137f4c153b8
Davi Thiesse <davithss@gmail.com>
2011-05-07 14:47:36 -0300

    called find_all_categories
    Signed-off-by: Davi Thiesse <davithss@gmail.com>

--- app/controllers/admin/real_estates_controller.rb
+++ app/controllers/admin/real_estates_controller.rb
@@ -3,6 +3,13 @@ module Admin
 
     crudify :real_estate,
             :title_attribute => 'title', :xhr_paging => true
+            
+    before_filter :find_all_categories,
+                  :only => [:new, :edit, :create, :update]
 
+  protected
+   def find_all_categories
+       @real_estates_categories = Category.order('position ASC')
+   end
   end
 end
